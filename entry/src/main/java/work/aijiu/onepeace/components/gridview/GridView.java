3334a9cb64a78fb3be6b19bda2d3439484fc91f2
Tiger <liuhongjian0316@outlook.com>
2022-03-22 16:47:07 +0800

    添加按钮点击水波纹 添加切换不同状态图标按钮

---
+++ entry/src/main/java/work/aijiu/onepeace/components/gridview/GridView.java
@@ -0,0 +1,45 @@
+package work.aijiu.onepeace.components;
+
+import ohos.agp.components.AttrSet;
+import ohos.agp.components.Component;
+import ohos.agp.components.TableLayout;
+import ohos.app.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GridView extends TableLayout {
+
+    private List<Component> list = new ArrayList<>();
+
+    public GridView(Context context) {
+        super(context);
+    }
+
+    public GridView(Context context, AttrSet attrSet) {
+        super(context, attrSet);
+    }
+
+    public GridView(Context context, AttrSet attrSet, String styleName) {
+        super(context, attrSet, styleName);
+    }
+
+
+
+
+    /**
+     * The setAdapter
+     *
+     * @param adapter             adapter
+     * @param longClickedListener longClickedListener
+     */
+    void setAdapter(GridAdapter adapter, LongClickedListener longClickedListener, ClickedListener clickedListener) {
+        removeAllComponents();
+        this.list = adapter.getComponentList();
+        for (int i = 0; i < adapter.getComponentList().size(); i++) {
+            adapter.getComponentList().get(i).setLongClickedListener(longClickedListener);
+            adapter.getComponentList().get(i).setClickedListener(clickedListener);
+            addComponent(adapter.getComponentList().get(i));
+        }
+    }
+}
